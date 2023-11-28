26af8f87bbc1d5328aa2ecea88969100f1079ce8
rv-us <rverma8@ucsc.edu>
2023-11-27 18:04:29 -0800

    Lets get this Party Started

--- random_number.py
+++ random_number.py
@@ -1,4 +1,9 @@
 import random
 
 # random.randint(a,b) generates a random number between a and b inclusive.
-print(f"The random number is {random.randint(0,9)}")
+print(f"The random number is ur mom {random.randint(0,9)}")
+print("Bye World")
+print("the cruel cruel world...")
+while True:
+    print("Ruthwika is Stupid")
+    print("I will cause the merge conflict")

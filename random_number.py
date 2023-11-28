9a51e553497ef97d280b4fe9cb62868631c3f8fb
Ruthwika Gajjala <rgajjala@ucsc.edu>
2023-11-27 18:03:24 -0800

    genius code by ruthwika

--- random_number.py
+++ random_number.py
@@ -2,3 +2,11 @@ import random
 
 # random.randint(a,b) generates a random number between a and b inclusive.
 print(f"The random number is {random.randint(0,9)}")
+num = 18
+print(f"Ruthwika is {num} years old!")
+print("mwahahahahahahah")
+while num < 0:
+    print("DONT BELIEVE RACHIT")
+    print("RUTHWIKA IS THE BEST")
+    print("READ WEBTOON AT MIDNIGHT")
+    num -= 1
